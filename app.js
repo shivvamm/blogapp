527139b8f6f776a1e0f4812bbbe9147a2c9c0ecc
Shivam Pandey <ayushidixit108@gmail.com>
2022-06-01 15:36:11 +0530

    added router

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var studentRouter = require('./routes/student');
-var usersRouter = require('./routes/teacher');
+//var usersRouter = require('./routes/teacher');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/student', studentRouter);
-app.use('/teacher', usersRouter);
+//app.use('/teacher', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
