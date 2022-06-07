210bf076ba2b515176c485746f0a6c566013a439
Shivam Pandey <shivamp272btechcse2024@kccitm.edu.in>
2022-06-07 13:00:05 +0530

    Sended email pass with post method

--- routes/login.js
+++ routes/login.js
@@ -6,10 +6,17 @@ router.get('/', function(req, res) {
    res.render('login');
 });
 
-router.get('/getLogin', function(req, res) {
+// router.get('/getLogin', function(req, res) {
+//     console.log(req.query);
+//     // res.send({"name":"shivam"});
+//     res.json({"EMAIL":req.query.email,"PASS":req.query.password});
+//  });
+
+router.post('/getLogin', function(req, res) {
     console.log(req.query);
     // res.send({"name":"shivam"});
-    res.json({"EMAIL":req.query.email,"PASS":req.query.password});
+    res.json({"EMAIL":req.body.email,"PASS":req.body.password});
  });
+
 
 module.exports = router;
