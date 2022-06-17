a55d1e3a97e35c7fe05ffd02f77d68af1d416ed5
Shivam Pandey <shivamp272btechcse2024@kccitm.edu.in>
2022-06-17 12:04:31 +0530

    Used Map  and groupby render jason on index

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,15 @@ var _ = require('underscore');
 router.get('/', function(req, res) {
   // res.render('index', { title: 'Express' });
   console.log(req);
-  res.json({name:"Shivam"});
+  // res.json({name:"Shivam"});
+  res.json({Groupby:_.groupBy(['one', 'two', 'three'], 'length'),Map:_.map([1, 2, 3], function(num){ return num * 3; })}),
   console.log("Map"),
   console.log(_.map([1,2,3])),
   console.log(_.map([1, 2, 3], function(num){ return num * 3; })),
   console.log("Groupby"),
   console.log(_.groupBy([1.3, 2.1, 2.4])),
-  console.log(_.groupBy([1.3, 2.1, 2.4], function(num){ return Math.floor(num); }))
+  console.log(_.groupBy([1.3, 2.1, 2.4], function(num){ return Math.floor(num)})),
+  console.log(_.groupBy(['one', 'two', 'three'], 'length'))
 });
 
 // router.get('/:da',function(req, res){
