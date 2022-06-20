f5b67b5e3f5b9c6ce712bd485ba708673dec1797
Shivam Pandey <shivam272btechcse2024@kccitm.edu.in>
2022-06-20 13:55:29 +0530

    DONE CALCULATOR

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,44 @@ var router = express.Router();
 var _ = require('underscore');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  // res.render('index', { title: 'Express' });
-  console.log(req);
-  // res.json({name:"Shivam"});
-  res.json({Groupby:_.groupBy(['one', 'two', 'three'], 'length'),Map:_.map([1, 2, 3], function(num){ return num * 3; })}),
-  console.log("Map"),
-  console.log(_.map([1,2,3])),
-  console.log(_.map([1, 2, 3], function(num){ return num * 3; })),
-  console.log("Groupby"),
-  console.log(_.groupBy([1.3, 2.1, 2.4])),
-  console.log(_.groupBy([1.3, 2.1, 2.4], function(num){ return Math.floor(num)})),
-  console.log(_.groupBy(['one', 'two', 'three'], 'length'))
+// router.get('/', function(req, res) {
+//   // res.render('index', { title: 'Express' });
+//   console.log(req);
+//   // res.json({name:"Shivam"});
+//   res.json({Groupby:_.groupBy(['one', 'two', 'three'], 'length'),Map:_.map([1, 2, 3], function(num){ return num * 3; })}),
+//   console.log("Map"),
+//   console.log(_.map([1,2,3])),
+//   console.log(_.map([1, 2, 3], function(num){ return num * 3; })),
+//   console.log("Groupby"),
+//   console.log(_.groupBy([1.3, 2.1, 2.4])),
+//   console.log(_.groupBy([1.3, 2.1, 2.4], function(num){ return Math.floor(num)})),
+//   console.log(_.groupBy(['one', 'two', 'three'], 'length'))
+// });
+
+router.get('/cal',function(req, res){
+  res.render('index');
 });
 
-// router.get('/:da',function(req, res){
-//   res.render('index',{title:req.params.da});
-// });
+router.all('/cal/add',function(req, res){
+  var x=parseInt(req.body.num1);
+   var  y=parseInt(req.body.num2);
+    var result=x+y
+  res.render('index',result);
+});
+
+router.all('/cal/sub',function(req, res){
+  var x=parseInt(req.body.num1);
+    var y=parseInt(req.body.num2);
+    var resutl=x-y
+  res.render('index',result);
+});
+
+router.all('/cal/mul',function(req, res){
+  var x=parseInt(req.body.num1);
+    var y=parseInt(req.body.num2);
+    var result=x*y
+  res.render('index',result);
+});
+
+
 module.exports = router;
