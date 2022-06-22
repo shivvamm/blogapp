97909a6298a357efa6f7c8feb7fcdad1000e4882
Shivam Pandey <shivam272btechcse2024@kccitm.edu.in>
2022-06-22 15:51:53 +0530

    calculator with result table

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var _ = require('underscore');
+var mysql = require('mysql');
 
 /* GET home page. */
 // router.get('/', function(req, res) {
@@ -17,9 +18,43 @@ var _ = require('underscore');
 //   console.log(_.groupBy(['one', 'two', 'three'], 'length'))
 // });
 
+var pool = mysql.createPool({
+  connectionLimit: 10,
+  host : 'easylearning.guru',
+  user :  'kcc_student',
+  password : 'Kccitm.edu.in1',
+  database : 'kccStudent'
+});
+
 router.get('/cal',function(req, res){
+  pool.getConnection(function (err,connection){
+    connection.query("SELECT * FROM   RESULTS ORDER BY id",function(err,rows){
+       connection.release();
+       if(err) throw err;
+
+       console.log(rows.length);
+    });
+ });
   res.render('index');
 });
+
+
+
+
+router.get('/read', function(req, res) {
+  pool.getConnection(function (err,connection){
+     connection.query("SELECT * FROM SHIVAMP",function(err,rows){
+        connection.release();
+        if(err) throw err;
+
+        console.log(rows.length);
+        res.send(JSON.stringify(rows));
+     });
+  });
+ 
+});
+
+
 
 // router.all('/cal/add',function(req, res){
 //     var result=x+y;
@@ -44,11 +79,20 @@ router.get('/cal',function(req, res){
 router.all('/add',function(req, res){
   console.log("ADDITION");
   console.log(req.body);
+  var o = "+"
   var x = parseInt(req.body.a);
   var y =parseInt(req.body.b);
   var result =x+y;
   console.log(result);
   res.json(result);
+  pool.getConnection(function (err,connection){
+    connection.query("INSERT INTO RESULTS (NUM1, NUM2 , OP,  RES) VALUES ('"+x+"', '"+y+"','"+o+"','"+result+"')",function(err,rows){
+       connection.release();
+       if(err) throw err;
+       console.log(rows.length);
+       res.send(JSON.stringify(rows));
+    });
+ });
 });
 
 router.all('/sub',function(req, res){
